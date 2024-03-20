c51961dbdce07e792e6b6afbc9161e76680bfd2e
greg-vbase <greg+github@vbase.com>
2024-03-19 18:49:00 -0700

    Allow notebook to run outside of collab

--- samples/collab_utils.py
+++ samples/collab_utils.py
@@ -2,20 +2,23 @@
 Google Collab utilities
 """
 
-from google.colab import userdata
 import os
 from typing import List
 
 
-def add_user_secrets_to_env(userdata_keys: List[str]):
+def try_add_user_secrets_to_env(userdata_keys: List[str]):
     """
-    Add google.collab.userdata secrets to the environment.
+    Add google.collab.userdata secrets to the environment, if possible.
     This allows vBase configuration to be stored in Collab user secrets.
 
     :param userdata_keys: The keys for environment variables to import from userdata.
     """
-    # Loop through the secrets and set them as environment variables
-    for k in userdata_keys:
-        v = userdata.get(k)
-        os.environ[k] = v
-        print(f"Imported {k}")
+    try:
+        from google.colab import userdata
+        # Loop through the secrets and set them as environment variables
+        for k in userdata_keys:
+            v = userdata.get(k)
+            os.environ[k] = v
+            print(f"Imported {k}")
+    except ImportError:
+        pass
